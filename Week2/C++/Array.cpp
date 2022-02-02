9033c4120006f67043dc4957d1c8ea57b136f219
asegura4488 <asegura4488@gmail.com>
2022-02-02 13:36:52 -0500

    C++

---
+++ Week2/C++/Array.cpp
@@ -0,0 +1,35 @@
+#include <iostream> // imprimir a pantalla
+
+//using namespace std; No hacer con Alejandro Segura
+
+// primera funcion 
+int GetNumber(int a=10){
+ 
+ return a;	
+
+ }
+
+
+int main(){ // funcion principañ
+
+	// :: Operador de ambito
+	std::cout << " Mi programa " << std::endl;
+
+
+	double b = 0.;
+	for(int i = 0; i<=10; i++){
+		b += 1;
+
+		if (b < 5){
+		std::cout << GetNumber(b) <<std::endl;
+		}
+		else if ( b > 6){
+			std::cout << " Es mayor que 6 " << std::endl; 
+		}
+		//else
+        }
+	//<< operador de redireccionamiento
+
+
+	return 0;
+}
